beec1f381574d4a6a6a8f9170270a18cdfd1c93f
jesus_isao <jesusisao@gmail.com>
2020-01-19 22:56:19 +0900

    PHP追加

--- php-sample.php
+++ php-sample.php
@@ -3,4 +3,47 @@ function println($message)
 {
     echo $message.PHP_EOL;
 }
-println("hello world");
+
+// ----------------------------------------
+
+class SuperClass
+{
+    private $instanceVariable = "SuperClass";
+
+    public function superClassMethod()
+    {
+        println($this->instanceVariable);
+    }
+}
+
+class SubClass extends SuperClass
+{
+    private $instanceVariable = "SubClass";
+}
+
+// ----------------------------------------
+
+class SuperClassProtected
+{
+    protected $instanceVariable = "SuperClass";
+
+    public function superClassMethod()
+    {
+        println($this->instanceVariable);
+    }
+}
+
+class SubClassProtected extends SuperClassProtected
+{
+    protected $instanceVariable = "SubClass";
+}
+
+// ----------------------------------------
+
+println("---- SubClass ----");
+$sub = new SubClass();
+$sub->superClassMethod();
+
+println("---- SubClassProtected ----");
+$subp = new SubClassProtected();
+$subp->superClassMethod();
