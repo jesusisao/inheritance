851ff37093c214649127d31c3904405628a7a5c1
jesus_isao <jesusisao@gmail.com>
2020-01-20 00:50:41 +0900

    Dart追加

---
+++ dart-sample.dart
@@ -0,0 +1,33 @@
+void main() {
+  print("---- SubClassPrivate ----");
+  final subp = new SubClassPrivate();
+  subp.superClassMethod();
+
+  print("---- SubClass ----");
+  final sub = new SubClass();
+  sub.superClassMethod();
+}
+
+class SuperClassPrivate {
+  // アンダースコアをつけるとPrivateになるが、
+  // privateは同一ライブラリに対して可視のため、ここではサブクラスから普通に見えちゃう
+  final String _instanceVariable = "SuperClass";
+
+  void superClassMethod() => print(_instanceVariable);
+}
+
+class SubClassPrivate extends SuperClassPrivate {
+  final String _instanceVariable = "SubClass";
+}
+
+// ------------------------------------
+
+class SuperClass {
+  final String instanceVariable = "SuperClass";
+
+  void superClassMethod() => print(instanceVariable);
+}
+
+class SubClass extends SuperClass {
+  final String instanceVariable = "SubClass";
+}
